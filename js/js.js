97d594a25ed80f5a05a749a2fc1fe3e4a85f4006
Cris Stanza <crisstanza@users.noreply.github.com>
2018-04-13 14:30:10 -0300

    Create js.js

---
+++ js/js.js
@@ -0,0 +1,62 @@
+function addClass(element, className) {
+	var classNames = element.getAttribute('class');
+	if (classNames) {
+		var classes = classNames.split(' ');
+		var length = classes.length;
+		for (var i = 0 ; i < length ; i++) {
+			var clazz = classes[i];
+			if (clazz == className) {
+				return;
+			}
+		}
+		var newClassNames = classNames.trim() + ' ' + className;
+		element.className = newClassNames;
+	} else {
+		element.className = className;	
+	}
+}
+
+function delClass(element, className) {
+	var classNames = element.getAttribute('class');
+	if (classNames) {
+		var classes = classNames.split(' ');
+		var length = classes.length;
+		for (var i = 0 ; i < length ; i++) {
+			var clazz = classes[i];
+			if (clazz == className) {
+				classes[i] = '';
+				break;
+			}
+		}
+		var newClassNames = classes.join(' ').trim();
+		element.className = newClassNames;
+	}
+}
+
+function hasClass(element, className) {
+	var classNames = element.className;
+	if (classNames) {
+		var classes = classNames.split(' ');
+		var length = classes.length;
+		for (var i = 0 ; i < length ; i++) {
+			var clazz = classes[i];
+			if (clazz == className) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+(function() {
+
+	function initGlobals() {
+	}
+
+	function init() {
+		initGlobals();
+	}
+
+	window.addEventListener('load', function() { init(); });
+
+})();
